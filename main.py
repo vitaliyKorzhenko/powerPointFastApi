93ac1969b58de5333167e52a14368b235eb076da
Vitaliy <vitaliykorzenkoua@gmail.com>
2024-03-15 15:51:29 +0200

    go test
    go test

--- main.py
+++ main.py
@@ -37,31 +37,26 @@ def parse_pptx(prs):
     return images
 
 def replace_image_in_presenation(prs, media_uniq_name, new_image_stream):
-    print('new image path')
-    imageFind = False
     for slide in prs.slides:
         for shape in slide.shapes:
-            print(shape.shape_type)
-            print(shape.name)
             # print(old_image_name)
             if shape.shape_type == 13:
-                print('found image')
-                print(shape.name)
                 name = shape.image.filename.split('.')[0]
                 current_uniq_name = name + str(shape.shape_id) + '.' + shape.image.ext
-                print(current_uniq_name)
-                print('Find image', media_uniq_name);
             if current_uniq_name == media_uniq_name:
-                slide_part, rId = shape.part, shape._element.blip_rId
-                image_part = slide_part.related_part(rId)    
-                image_part.blob = new_image_stream.read()
-                imageFind = True
+                print('Find REPLACE IMAGE', media_uniq_name);
+                try:
+                    x = 5;
+                    slide_part, rId = shape.part, shape._element.blip_rId
+                    image_part = slide_part.related_part(rId)
+                    new_image_stream.seek(0) 
+                    image_part.blob = new_image_stream.read()
+                    new_image_stream.close()
+                except Exception as e:
+                    print('Error READ', e)
                 break
     #create random name for new presentation       
-    if imageFind:
-        return prs
-    else:
-        return None
+    return prs
 
 @app.post("/presentation/parsing")
 async def parse_presentation(name: str):
@@ -118,70 +113,47 @@ async def replace_image(presentationInfo: PresentationParams):
     imageBucket = 'img'
     presentation_name = presentationInfo.presentation
     bucket = BucketManager()
-    if (bucket.file_exists(templatesBucket, presentation_name)):
+    
+    if bucket.file_exists(templatesBucket, presentation_name):
         file = bucket.getObjectBody(templatesBucket + '/' + presentation_name)
         file_stream = BytesIO(file)
         presentation = pptx.Presentation(file_stream)
         newName = presentationInfo.resultFileName + '.pptx'
 
-        #use for loop to replace all images in replacement array
         result_stream = BytesIO()
-        print("BEFORE LOOP")
-    for item in presentationInfo.replacements:
-        print("ITEM", item)
-        if (item['media_unique_name'] != None and item['assets_file'] != None):
-            print("GO GO GO", item['media_unique_name'], item['assets_file'])
-            if (bucket.file_exists(imageBucket, item["assets_file"])):
-                print("exist image")
-                image = bucket.getObjectBody(imageBucket + '/' + item['assets_file'])
-                if (image == None):
-                    return HTTPException(status_code=404, detail="Image not found")
-                image_stream = BytesIO(image)
-                result_prs = replace_image_in_presenation(presentation, item['media_unique_name'], image_stream)
-                if (result_prs == None):
-                    return HTTPException(status_code=404, detail="Image not found")
-                else:
-                    #save result presentation to results folder
-                    print(newName)
-                    #save to results folder
-                    #bucket.put_object('results/' + newName, result_prs);
-                    result_prs.save(result_stream)
-                    result_stream.seek(0)
-        else:
-            return HTTPException(status_code=404, detail="Image not found")
-    
+
+        for item in presentationInfo.replacements:
+            if item.get('media_unique_name') and item.get('assets_file'):
+                print("GO GO GO", item['media_unique_name'], item['assets_file'])
+                if bucket.file_exists(imageBucket, item["assets_file"]):
+                    image = bucket.getObjectBody(imageBucket + '/' + item['assets_file'])
+                    if not image:
+                        return HTTPException(status_code=404, detail="Image not found")
+                    print("start create image stream")
+                    byteImgIO = BytesIO(image)
+                    byteImgIO.seek(0)
+                    with byteImgIO as image_stream:
+                        result_prs = replace_image_in_presenation(presentation, item['media_unique_name'], image_stream)
+                        if not result_prs:
+                            return HTTPException(status_code=404, detail="Image not found")
+                        else:
+                            # Update presentation
+                            print("UPDATE PRESENTATION")
+                            presentation = result_prs
+                            #delete byteImgIO
+                    
+            else:
+                return HTTPException(status_code=404, detail="Invalid replacement item")
+
+        # Save updated presentation to results folder
+        print("before loop");
+        presentation.save(result_stream)
+        result_stream.seek(0)
         bucket.put_object(data=result_stream.read(), key='pptx/' + newName)
         return {"status": "success", "file": newName}
 
     else:
-        return HTTPException(status_code=404, detail="File not found")
+        return HTTPException(status_code=404, detail="Presentation file not found")
+
 
    
-@app.get("/presentation/test")
-async def test():
-     print("start init");
-     configPath = 'config.ini'
-     config = configparser.ConfigParser()
-     config.read(configPath)
-     bucket = BucketManager()
-     bucketInfo = bucket.get_bucket_info()
-     print(bucketInfo)
-     folders = bucket.get_all_folders()
-     print(folders)
-     #test folder 
-     folder = "templates"
-     files = bucket.get_files(folder)
-     testFile = files[1];
-     print("TEST FILE", testFile)
-     testBody = bucket.getObjectBody(testFile)
-     file_stream = BytesIO(testBody)
-     testPptx = pptx.Presentation(file_stream)
-     images = parse_pptx(testPptx)
-     
-     return {
-            "bucket": config['s3']['bucket'],
-            "info": bucketInfo,
-            "folders": folders,
-            "file_path": testFile,
-            "images": images,
-     }
